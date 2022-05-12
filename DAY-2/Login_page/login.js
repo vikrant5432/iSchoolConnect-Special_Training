d60c133a2241bc33ef92e151ed0f380d467c3ef3
Vikrant kumar <42002633+vikrant5432@users.noreply.github.com>
2022-05-12 17:10:19 +0530

    Add files via upload

---
+++ DAY-2/Login_page/login.js
@@ -0,0 +1,123 @@
+const mysql = require("mysql2");
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+const { response } = require("express");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "nodelogin",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log("error in DB");
+  } else {
+    console.log("connected to DB");
+  }
+});
+
+const app = express();
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "static")));
+app.use(express.static(path.join(__dirname, "assignment")));
+// http://localhost:3306/
+app.get("/", function (request, response) {
+  // Render login template
+  response.sendFile(path.join(__dirname + "/login.html"));
+});
+
+app.get("/signup", (req, res) => {
+  res.sendFile(path.join(__dirname + "/assignment/signp.html"));
+});
+// http://localhost:3306/auth
+app.post("/auth", function (request, response) {
+  // Capture the input fields
+  let username = request.body.username;
+  let password = request.body.password;
+  // Ensure the input fields exists and are not empty
+  if (username && password) {
+    // Execute SQL query that'll select the account from the database based on the specified username and password
+    connection.query(
+      "SELECT * FROM account WHERE username = ? AND password = ?",
+      [username, password],
+      function (error, results, fields) {
+        // If there is an issue with the query, output the error
+        if (error) throw error;
+        // If the account exists
+        if (results.length > 0) {
+          // Authenticate the user
+          request.session.loggedin = true;
+          request.session.username = username;
+          // Redirect to home page
+          response.redirect("/home");
+        } else {
+          response.send("Incorrect Username and/or Password!");
+        }
+        response.end();
+      }
+    );
+  } else {
+    response.send("Please enter Username and Password!");
+    response.end();
+  }
+});
+
+//http://localhost:3000/auth-sign
+app.post("/auth-signup", (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if (username && password) {
+    connection.query(
+      "SELECT * FROM account WHERE username = ? AND password = ?",
+      [username, password],
+      function (err, results, fields) {
+        // If there is an issue with the query, output the error
+        if (err) throw err;
+        // If the account exists
+        if (results.length > 0) {
+          res.send("User already exist!!");
+          // console.log("user exist!");
+        } else {
+          connection.query(
+            "INSERT INTO account(UserName,password) VALUES(?,?)",
+            [username, password],
+            function (err, result, fields) {
+              if (err) throw err;
+              res.redirect("/");
+            }
+          );
+        }
+        res.end();
+      }
+    );
+  } else {
+    res.send("Please enter Username and Password!");
+    res.end();
+  }
+});
+
+// http://localhost:3306/home
+app.get("/home", function (request, response) {
+  // If the user is loggedin
+  if (request.session.loggedin) {
+    // Output username
+    response.send("Welcome back, " + request.session.username + "!");
+  } else {
+    // Not logged in
+    response.send("Please login to view this page!");
+  }
+  response.end();
+});
+app.listen(3000, console.log("server is on localhost:3000"));
